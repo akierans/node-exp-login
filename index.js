dc6415e1159d63ad5e35cdfadde095df05ddb592
akierans <akierans@gmail.com>
2020-08-10 21:20:43 +0100

    Added up to session config in index.js

--- index.js
+++ index.js
@@ -5,12 +5,29 @@
  const express = require("express");
  const path = require("path");
 
+ const expressSession = require("express-session");
+ const passport = require("passport");
+ const Auth0Strategy = require("passport-auth0");
+
+ require("dotenv").config();
+
  /**
  * App Variables
  */
 
  const app = express();
  const port = process.env.PORT || "8000";
+
+ const app = express();
+ const port = process.env.PORT || "8000";
+
+ /**
+ * Session Configuration
+ */
+
+  /**
+ * Passport Configuration
+ */
 
  /**
  * App Configuration
