daf3ce682cdc73de6918d0782a134c259cdb9bf9
akierans <akierans@gmail.com>
2020-08-11 18:40:04 +0100

    Added template auth.js

---
+++ auth.js
@@ -0,0 +1,22 @@
+/**
+  * Required External Modules
+  */
+
+const express = require("express");
+const router = express.Router();
+const passsport = require("passport");
+const util require("util");
+const url = require("url");
+const querystring = require("querystring");
+
+require("dotenv").config();
+
+
+/**
+  * Routes Definitions
+  */
+
+
+/**
+  * Module Exports
+  */
