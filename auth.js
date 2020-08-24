85bc0e3d97eb200d230fb7e07ef397bc889eb5c9
akierans <akierans@gmail.com>
2020-08-24 17:55:31 +0100

    Fixed issue with login not being recognised, missing middleware to expose user to app res.locals

--- auth.js
+++ auth.js
@@ -11,70 +11,68 @@ const querystring = require("querystring");
 
 require("dotenv").config();
 
+const authRouter = require("./auth");
 
 /**
   * Routes Definitions
   */
 
-  router.get(
-  	"/login",
-  	passport.authenticate("auth0", {
-  		scope: "openid email profile"
-  	}),
-  	(req, res) => {
-  		res.redirect("/");
-  	}
+router.get(
+  "/login",
+  passport.authenticate("auth0", {
+    scope: "openid email profile"
+  }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
+
+router.get("/callback", (req, res, next) => {
+  passport.authenticate("auth0", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect("/login");
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      const returnTo = req.session.returnTo;
+      delete req.session.returnTo;
+      res.redirect(returnTo || "/");
+    });
+  })(req, res, next);
+});
+
+router.get("/logout", (req, res) => {
+  req.logOut();
+
+  let returnTo = req.protocol + "://" + req.hostname;
+  const port = req.connection.localPort;
+
+  if (port !== undefined && port !== 80 && port !== 443) {
+    returnTo =
+      process.env.NODE_ENV === "production"
+        ? `${returnTo}/`
+        : `${returnTo}:${port}/`;
+  }
+
+  const logoutURL = new URL(
+    util.format("https://%s/logout", process.env.AUTH0_DOMAIN)
   );
-
-  router.get("/callback", (req, res, next) => {
-  	passport.authenticate("auth0", (err, user, info) => {
-  		if (err) {
-  			return next(err);
-  		}
-  		if (!user) {
-  			return res.redirect("/login");
-  		}
-  		req.logIn(user, (err) => {
-  			if (err) {
-  				return next(err);
-  			}
-  			const returnTo = req.session.returnTo;
-  			delete req.session.returnTo;
-  			res.redirect(returnTo || "/");
-  		});
-  	})(req, res, next);
+  const searchString = querystring.stringify({
+    client_id: process.env.AUTH0_CLIENT_ID,
+    returnTo: returnTo
   });
+  logoutURL.search = searchString;
 
-  router.get("/logout", (req, res) => {
-  	req.logOut();
-
-  	let returnTo = req.protocol + "://" + req.hostname;
-  	const port = req.connection.localPort;
-
-  	//If local append port to redirect URL
-  	if (port !== undefined && port !== 80 && port !== 443) {
-  		returnTo =
-  		process.env.NODE_ENV === "production"
-  			? `${returnTo}/`
-  			: `${returnTo}:${port}/`;
-  	}
-
-  	//Replace %s with value of process.env.AUTH0_DOMAIN
-  	const logoutURL = new URL(
-  		util.format("https://%s/logout", process.env.AUTH0_DOMAIN)
-  	);
-  	const searchString = querystring.stringify({
-  		client_id: process.env.AUTH0_CLIENT_ID,
-  		returnTo: returnTo
-  	});
-  	logoutURL.search = searchString;
-
-  	res.redirect(logoutURL);
-  });
-
+  res.redirect(logoutURL);
+});
 
 /**
   * Module Exports
   */
 
-  module.exports = router;
+module.exports = router;
